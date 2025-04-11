28c27374e5d951a8b3eb05a19441d5488bc55f36
ErinLee8245 <erinlee@wemade.com>
2025-04-11 20:44:35 +0900

    불필요한 코드 정리

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// const { default: SAXParser } = require("./SAXParser/sax-parser");
-// import * as SAXParser from './SAXParser/sax-parser';
-
 import { SAXParser } from "./SAXParser/sax-parser.ts";
 
 // 이벤트 발생될 때 -> 읽기
@@ -11,6 +8,3 @@ console.log(new SAXParser('document.html').querySelectors('[style="width:30px"]'
 console.log('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ');
 console.log(new SAXParser('document.html').getElementByTag('h2'))
 console.log('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ');
-
-
-// new SAXParser('document.html')
